10ade7dc976f8c41fa5209a5244692248a1de377
lee jiho <leejiho999o@gmail.com>
2020-07-13 02:23:01 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 
 //以下削除
