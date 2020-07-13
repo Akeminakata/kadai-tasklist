7231b7fc07433145b334c6167816cd397239f9ad
lee jiho <leejiho999o@gmail.com>
2020-07-13 08:11:40 +0000

    user login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-tends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
     <div class="text-center">
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,20 @@
 |
 */
 
+Route::get('/', 'TasksController@index');
+Route::resource('tasks', 'TasksController');
+
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// 認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-
-//以下削除
-//Route::get('/', 'TasksController@index');
-//Route::resource('tasks', 'TasksController');
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'TaskController', ['only' => ['index', 'show', 'destroy']]);
+});
 
 // CRUD
 // メッセージの個別詳細ページ表示
