fd2459af05c6ed5f26601bfb9f3e408ac316b5e9
lee jiho <leejiho999o@gmail.com>
2020-07-13 01:43:30 +0000

    user register

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,13 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-Route::resource('tasks', 'TasksController');
+
+//以下削除
+//Route::get('/', 'TasksController@index');
+//Route::resource('tasks', 'TasksController');
 
 // CRUD
 // メッセージの個別詳細ページ表示
